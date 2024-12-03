3f069f94ac4436cb4ce885beb4f4c614123fe21b
rajuboppa <rajupatelboppa@gmail.com>
2024-12-03 00:38:26 -0500

    custom login page security config

---
+++ src/main/java/com/raju/spring_mvc_security/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.raju.spring_mvc_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginPage() {
+        return "login-page";
+    }
+}
---
+++ src/main/java/com/raju/spring_mvc_security/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.raju.spring_mvc_security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest()
+                        .authenticated()
+                ).formLogin(form ->
+                form.loginPage("/showMyLoginPage")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll());
+        return http.build();
+
+    }
+}
