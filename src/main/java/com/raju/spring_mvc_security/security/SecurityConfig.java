4ce9755870f6916bdc9a0e50df28622c1d27090e
rajuboppa <rajupatelboppa@gmail.com>
2024-12-03 01:40:56 -0500

    using roles from the database. ANd wrapping up Spring MVC security config to enable role based access

--- src/main/java/com/raju/spring_mvc_security/security/SecurityConfig.java
+++ src/main/java/com/raju/spring_mvc_security/security/SecurityConfig.java
@@ -7,20 +7,20 @@ import org.springframework.security.config.annotation.web.configurers.LogoutConf
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-
-        UserDetails john = User.withUsername("john").password("{noop}test123").roles("EMPLOYEE").build();
-        UserDetails mary = User.withUsername("mary").password("{noop}test123").roles("EMPLOYEE", "MANAGER").build();
-        UserDetails susan = User.withUsername("susan").password("{noop}test123").roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
